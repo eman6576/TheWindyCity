ec0c63ddfbfdf17b6bbcc3d9bfeb0fb35338ea55
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-03 14:11:37 -0500

    Renamed the file.

---
+++ TheWindyCity/MenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TheWindyCity
+//
+//  Created by Emanuel  Guerrero on 3/2/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    @IBOutlet weak var checkItOutButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        checkItOutButton.layer.cornerRadius = 5.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
