7dc28b4f5e0ece4508be419f0f0df3a9f8f03720
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-07 18:32:41 -0500

    Added a new view controller class to manage the Photos story board.

---
+++ TheWindyCity/PhotosViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PhotosViewController.swift
+//  TheWindyCity
+//
+//  Created by Emanuel  Guerrero on 3/7/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
