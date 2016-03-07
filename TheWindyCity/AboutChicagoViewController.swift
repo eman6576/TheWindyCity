464f54b0b161814e42c030145fb3699d67309fe8
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-07 18:31:04 -0500

    Added a new view controller class to manage the About Chicago story board.

---
+++ TheWindyCity/AboutChicagoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutChicagoViewController.swift
+//  TheWindyCity
+//
+//  Created by Emanuel  Guerrero on 3/7/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class AboutChicagoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
